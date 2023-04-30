165e3ddca0c7c6c6d0bdfd860b9335ee93103a0c
saiprasad_barke <barke.saiprasad1992@gmail.com>
2023-04-30 17:31:50 +0200

    Added file mvsnet_components.py - Added class FeatureNet - Added classs CostRegNet

---
+++ rmvd/models/blocks/mvsnet_components.py
@@ -0,0 +1,83 @@
+# Local imports
+from .mvsnet_convbnrelu import ConvBnReLU, ConvBnReLU3D
+
+# External imports
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class FeatureNet(nn.Module):
+    def __init__(self):
+        super(FeatureNet, self).__init__()
+        self.inplanes = 32
+
+        self.conv0 = ConvBnReLU(3, 8, 3, 1, 1)
+        self.conv1 = ConvBnReLU(8, 8, 3, 1, 1)
+
+        self.conv2 = ConvBnReLU(8, 16, 5, 2, 2)
+        self.conv3 = ConvBnReLU(16, 16, 3, 1, 1)
+        self.conv4 = ConvBnReLU(16, 16, 3, 1, 1)
+
+        self.conv5 = ConvBnReLU(16, 32, 5, 2, 2)
+        self.conv6 = ConvBnReLU(32, 32, 3, 1, 1)
+        self.feature = nn.Conv2d(32, 32, 3, 1, 1)
+
+    def forward(self, x):
+        x = self.conv1(self.conv0(x))
+        x = self.conv4(self.conv3(self.conv2(x)))
+        x = self.feature(self.conv6(self.conv5(x)))
+        return x
+
+
+class CostRegNet(nn.Module):
+    def __init__(self):
+        super(CostRegNet, self).__init__()
+        self.conv0 = ConvBnReLU3D(32, 8)
+
+        self.conv1 = ConvBnReLU3D(8, 16, stride=2)
+        self.conv2 = ConvBnReLU3D(16, 16)
+
+        self.conv3 = ConvBnReLU3D(16, 32, stride=2)
+        self.conv4 = ConvBnReLU3D(32, 32)
+
+        self.conv5 = ConvBnReLU3D(32, 64, stride=2)
+        self.conv6 = ConvBnReLU3D(64, 64)
+
+        self.conv7 = nn.Sequential(
+            nn.ConvTranspose3d(
+                64, 32, kernel_size=3, padding=1, output_padding=1, stride=2, bias=False
+            ),
+            nn.BatchNorm3d(32),
+        )
+
+        self.conv9 = nn.Sequential(
+            nn.ConvTranspose3d(
+                32, 16, kernel_size=3, padding=1, output_padding=1, stride=2, bias=False
+            ),
+            nn.BatchNorm3d(16),
+        )
+
+        self.conv11 = nn.Sequential(
+            nn.ConvTranspose3d(
+                16, 8, kernel_size=3, padding=1, output_padding=1, stride=2, bias=False
+            ),
+            nn.BatchNorm3d(8),
+        )
+
+        self.prob = nn.Conv3d(8, 1, 3, stride=1, padding=1)
+
+    def forward(self, x):
+        conv0 = self.conv0(x)
+        conv2 = self.conv2(self.conv1(conv0))
+        conv4 = self.conv4(self.conv3(conv2))
+        x = self.conv6(self.conv5(conv4))
+        x = conv4 + self.conv7(x)
+        del conv4
+        x = conv2 + self.conv9(x)
+        del conv2
+        x = conv0 + self.conv11(x)
+        del conv0
+        x = self.prob(x)
+        return x
